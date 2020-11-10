c45cd776e1d81c525a28db1ea952b8eb53a57850
Stephane Hercot <stephane.hercot@gmail.com>
2020-11-10 15:03:21 +0200

    some change to make the course work

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/StephaneHe/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('SH')
+            node / gitConfigEmail('stephane.hercot@gmail.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('StephaneHe/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
